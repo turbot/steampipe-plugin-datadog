8821553152e9975d5842e01ae2487eebe97abe8e
LalitTurbot <lalit@turbot.com>
2021-10-21 14:49:54 +0530

    update

--- datadog/table_datadog_monitor.go
+++ datadog/table_datadog_monitor.go
@@ -14,32 +14,32 @@ func tableDatadogMonitor(ctx context.Context) *plugin.Table {
 		Name:        "datadog_monitor",
 		Description: "Datadog monitor resource.",
 		List: &plugin.ListConfig{
-			Hydrate:    listMonitors,
+			Hydrate: listMonitors,
 			KeyColumns: plugin.KeyColumnSlice{
-				// {Name: "status", Require: plugin.Optional},
+				{Name: "name", Require: plugin.Optional},
 			},
 		},
 		Columns: []*plugin.Column{
 			// Top columns
 			{Name: "name", Type: proto.ColumnType_STRING, Description: "Name of the monitor."},
 			{Name: "id", Type: proto.ColumnType_STRING, Description: "ID of the monitor."},
-			{Name: "creator_email", Type: proto.ColumnType_STRING, Transform: transform.FromField("Creator.Email"), Description: "Email of the creator."},
 			{Name: "created_at", Type: proto.ColumnType_TIMESTAMP, Transform: transform.FromField("Created"), Description: "Timestamp of the monitor creation."},
+			{Name: "creator_email", Type: proto.ColumnType_STRING, Transform: transform.FromField("Creator.Email"), Description: "Email of the creator."},
 			{Name: "type", Type: proto.ColumnType_STRING, Description: "The type of the monitor. For more information about `type`, see the [monitor options](https://docs.datadoghq.com/monitors/guide/monitor_api_options/) docs."},
 
 			// Other useful columns
 			{Name: "message", Type: proto.ColumnType_STRING, Description: "Timestamp of the monitor creation."},
+			{Name: "modified_at", Type: proto.ColumnType_TIMESTAMP, Transform: transform.FromField("Modified"), Description: "Last timestamp when the monitor was edited."},
 			{Name: "multi", Type: proto.ColumnType_BOOL, Description: "Whether or not the monitor is broken down on different groups."},
 			{Name: "overall_state", Type: proto.ColumnType_STRING, Description: "Current state of the monitor."},
-			{Name: "modified_at", Type: proto.ColumnType_TIMESTAMP, Transform: transform.FromField("Modified"), Description: "Last timestamp when the monitor was edited."},
 			{Name: "priority", Type: proto.ColumnType_INT, Transform: transform.FromJSONTag().Transform(valueFromNullable), Description: "Integer from 1 (high) to 5 (low) indicating alert severity."},
 			{Name: "query", Type: proto.ColumnType_STRING, Description: "The monitor query."},
 
 			// JSON fields
 			{Name: "options", Type: proto.ColumnType_JSON, Description: "A list of role identifiers that can be pulled from the Roles API. Cannot be used with `locked` option."},
 			{Name: "restricted_roles", Type: proto.ColumnType_JSON, Description: "Relationships of the user object returned by the API."},
-			{Name: "state_groups", Type: proto.ColumnType_JSON, Transform: transform.FromField("State.Groups"), Description: "Dictionary where the keys are groups (comma separated lists of tags) and the values are the list of groups your monitor is broken down on."},
-			{Name: "tags", Type: proto.ColumnType_STRING, Description: "Tags associated to monitor."},
+			{Name: "group_states", Type: proto.ColumnType_JSON, Transform: transform.FromField("State.Groups"), Description: "Dictionary where the keys are groups (comma separated lists of tags) and the values are the list of groups your monitor is broken down on."},
+			{Name: "tags", Type: proto.ColumnType_JSON, Description: "Tags associated to monitor."},
 		},
 	}
 }
@@ -58,10 +58,10 @@ func listMonitors(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateDat
 	// page := int64(0) // int64 | The page to start paginating from. If this argument is not specified, the request returns all monitors without pagination.
 	opts := datadog.ListMonitorsOptionalParameters{}
 
-	// fiterStatus := d.KeyColumnQualString("status")
-	// if fiterStatus != "" {
-	// 	opts.FilterStatus = datadog.PtrString(fiterStatus)
-	// }
+	name := d.KeyColumnQualString("name")
+	if name != "" {
+		opts.Name = datadog.PtrString(name)
+	}
 
 	resp, _, err := apiClient.MonitorsApi.ListMonitors(ctx, opts)
 	if err != nil {
