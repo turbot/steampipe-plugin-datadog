8983ada85bb4c04dab3e73e910f1054b50b4c47a
LalitTurbot <lalit@turbot.com>
2021-10-25 15:27:24 +0530

    Add get call to dashboard table

--- datadog/table_datadog_dashboard.go
+++ datadog/table_datadog_dashboard.go
@@ -2,6 +2,7 @@ package datadog
 
 import (
 	"context"
+	"strings"
 
 	datadog "github.com/DataDog/datadog-api-client-go/api/v1/datadog"
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
@@ -13,6 +14,10 @@ func tableDatadogDashboard(ctx context.Context) *plugin.Table {
 	return &plugin.Table{
 		Name:        "datadog_dashboard",
 		Description: "Users in Datadog.",
+		Get: &plugin.GetConfig{
+			Hydrate:    getDashboard,
+			KeyColumns: plugin.SingleColumn("id"),
+		},
 		List: &plugin.ListConfig{
 			Hydrate: listDashboards,
 		},
@@ -26,7 +31,14 @@ func tableDatadogDashboard(ctx context.Context) *plugin.Table {
 			{Name: "is_read_only", Type: proto.ColumnType_BOOL, Description: "Indicates if the dashboard is read-only. If True, only the author and admins can make changes to it."},
 			{Name: "layout_type", Type: proto.ColumnType_STRING, Description: "Creation date of the dashboard."},
 			{Name: "modified_at", Type: proto.ColumnType_TIMESTAMP, Description: "Modification time of the dashboard."},
+			{Name: "reflow_type", Type: proto.ColumnType_STRING, Hydrate: getDashboard, Description: "DashboardReflowType Reflow type for a **new dashboard layout** dashboard. Set this only when layout type is 'ordered'. If set to 'fixed', the dashboard expects all widgets to have a layout, and if it's set to 'auto', widgets should not have layouts."},
 			{Name: "url", Type: proto.ColumnType_STRING, Description: "URL of the dashboard."},
+
+			// JSON columns
+			{Name: "restricted_roles", Type: proto.ColumnType_JSON, Hydrate: getDashboard, Description: "A list of role identifiers. Only the author and users associated with at least one of these roles can edit this dashboard. Overrides the `is_read_only` property if both are present."},
+			{Name: "template_variable_presets", Type: proto.ColumnType_JSON, Hydrate: getDashboard, Description: "List of template variables saved views."},
+			{Name: "template_variables", Type: proto.ColumnType_JSON, Hydrate: getDashboard, Description: "List of template variables for this dashboard."},
+			{Name: "widgets", Type: proto.ColumnType_JSON, Hydrate: getDashboard, Description: "List of widgets to display on the dashboard."},
 
 			// common fields
 			{Name: "title", Type: proto.ColumnType_STRING, Description: "Title of the dashboard."},
@@ -41,10 +53,8 @@ func listDashboards(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateD
 		return nil, err
 	}
 
-	// https://github.com/DataDog/datadog-api-client-go/blob/master/api/v2/datadog/docs/UsersApi.md#listusers
-	opts := datadog.ListDashboardsOptionalParameters{}
-
-	resp, _, err := apiClient.DashboardsApi.ListDashboards(ctx, opts)
+	// https://github.com/DataDog/datadog-api-client-go/blob/master/api/v1/datadog/docs/DashboardsApi.md#listdashboards
+	resp, _, err := apiClient.DashboardsApi.ListDashboards(ctx, datadog.ListDashboardsOptionalParameters{})
 	if err != nil {
 		plugin.Logger(ctx).Error("datadog_dashboard.listDashboards", "query_error", err)
 	}
@@ -58,4 +68,36 @@ func listDashboards(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateD
 	}
 
 	return nil, nil
+}
+
+func getDashboard(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	var dasboardID string
+	if h.Item != nil {
+		dasboardID = *h.Item.(datadog.DashboardSummaryDefinition).Id
+	} else {
+		dasboardID = d.KeyColumnQuals["id"].GetStringValue()
+	}
+
+	if strings.TrimSpace(dasboardID) == "" {
+		return nil, nil
+	}
+
+	plugin.Logger(ctx).Info("AM I HERE ################################")
+
+	ctx, apiClient, err := connectV1(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("datadog_dashboard.getDashboard", "connection_error", err)
+		return nil, err
+	}
+
+	resp, _, err := apiClient.DashboardsApi.GetDashboard(ctx, dasboardID)
+	if err != nil {
+		plugin.Logger(ctx).Error("datadog_dashboard.getDashboard", "query_error", err)
+		if err.Error() == "404 Not Found" {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return resp, nil
 }
